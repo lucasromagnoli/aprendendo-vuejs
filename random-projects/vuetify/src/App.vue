9facb339d89551d5172ad7dbcdca6531c4a1b0d8
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2020-12-02 23:16:22 -0300

    Treinando alertas

---
+++ random-projects/vuetify/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-card max-width="500px">
+          <v-card-title>Alertas</v-card-title>
+          <v-alert type="success">Mensagem de sucesso</v-alert>
+          <v-alert type="warning" icon="mdi-firework">Mensagem de aviso</v-alert>
+          <v-alert type="error" dismissible v-model="alertIsOpen" @input="alertClose">
+            Mensagem de erro com trigger no close
+          </v-alert>
+        </v-card>
+
+
+      </v-container>
+
+
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+  data() {
+    return {
+      alertIsOpen: true
+    }
+  },
+  methods: {
+    alertClose(vuetify) {
+      console.log(vuetify);
+    }
+  }
+};
+</script>
